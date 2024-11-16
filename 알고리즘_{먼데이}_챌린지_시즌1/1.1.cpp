bfcec02429d7a9df7ba2e77977af64cf3fd0e358
JIUUOO <nninjiuuoo@gmail.com>
2024-11-16 10:53:57 +0900

    solve 1.1

--- 알고리즘_{먼데이}_챌린지_시즌1/1.1.cpp
+++ 알고리즘_{먼데이}_챌린지_시즌1/1.1.cpp
@@ -1,29 +1,36 @@
 #include <algorithm>
-#include <cstring>
 #include <iostream>
-
-#define MAX 1000000
-#define LEN 20
+#include <vector>
 
 using namespace std;
 
-bool lexicographicalCompare(string word1, string word2) {
+bool lexicoCmp(string word1, string word2) {
   if (word1.length() != word2.length()) {
-    return min(word1.length(), word2.length());
+    return word1.length() < word2.length();
   } else {
-    return word1.compare(word2);
+    return word1.compare(word2) < 0;
   }
 }
 
 int main() {
 
   int n;
-  cin >> n;
+  int index;
+  cin >> n >> index;
 
-  char wordList[n][LEN];
+  vector<string> wordList(1000000);
   for (int i = 0; i < n; i++) {
     cin >> wordList[i];
   }
+
+  sort(wordList.begin(), wordList.begin() + n, lexicoCmp);
+
+  cout << endl;
+  for (int i = 0; i < n; i++) {
+    cout << wordList[i] << endl;
+  }
+
+  cout << wordList.at(index - 1) << endl;
 
   return 0;
 }
